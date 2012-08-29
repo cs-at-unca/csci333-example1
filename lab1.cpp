778b6a31cce4ad9ccea2850daa773ecdeeb8c23d
Semmy Purewal <semmypurewal@gmail.com>
2012-08-29 13:37:37 -0400

    add basic euler function

--- lab1.cpp
+++ lab1.cpp
@@ -3,7 +3,15 @@
 using std::cout;
 using std::endl;
 
+int euler(int n);
+
 int main() {
   cout << "hello world!" << endl;
+  euler(1000);
+  return 0;
+}
+
+int euler(int n) {
+  cout << "hello parameter: " << n << endl;
   return 0;
 }
